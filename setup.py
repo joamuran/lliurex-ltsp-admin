f847d806a3284c38aac96ac91eee6ba17d150edd
José A. Múrcia <joamuran@gmail.com>
2013-05-27 13:35:17 +0200

    adfvsd

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(  name            = "lliurex-ltsp-admin",
 	license		= "GPLv3",
 	platforms		= ['posix'],
 	package_dir      = {'': 'src'},
-	packages	=	find_packages(),
+	#packages	=	find_packages(),
 	py_modules         = ['LliureXLTSPAdmin', 'Browser']
 	)
 
