181c725e05de22b7c61d0b32e8ea732345fd44dd
José A. Múrcia <joamuran@gmail.com>
2013-05-28 09:49:18 +0200

    fixed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(  name            = "lliurex-ltsp-admin",
 	license		= "GPLv3",
 	platforms		= ['posix'],
 	package_dir      = {'': 'src'},
-	packages	=	['net.LliureX.LliureXLTSPAdmin.LliureXLTSPAdmin', 'net.LliureX.LliureXLTSPAdmin.Browser']
+	packages	=	['net.LliureX.LliureXLTSPAdmin', 'net.LliureX.LliureXLTSPAdmin']
 	#py_modules         = ['LliureXLTSPAdmin', 'Browser']
 	)
 
