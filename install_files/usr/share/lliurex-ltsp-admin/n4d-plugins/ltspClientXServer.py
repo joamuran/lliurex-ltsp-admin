d4cc239c7aedccc4eb7276bb411bed7700bc61ce
José Alfredo Múrcia Andrés <joamuran@gmail.com>
2013-11-25 09:35:41 +0100

    WIP

--- install_files/usr/share/lliurex-ltsp-admin/n4d-plugins/ltspClientXServer.py
+++ install_files/usr/share/lliurex-ltsp-admin/n4d-plugins/ltspClientXServer.py
@@ -10,10 +10,23 @@
 # Libraries
 import sys
 import subprocess
+import glob
+import os
+import string
+import time
+import signal
+
 
 
 
 class ltspClientXServer:
+	'''
+	Displays a graphical window to launch X applications into chroot
+	'''
+	
+	display=":42"
+	screen="1024x768x16"
+
 	def __init__(self):
 		'''
 		A simple init method
@@ -23,5 +36,183 @@ class ltspClientXServer:
 	def killXephyr(self, PID):
 		# kill instead of sudo kill, so, only running user can remove process
 		output=subprocess.check_output(["kill", "-9", str(PID)])
+
+
+	#########################################
+	# Let's go with X-Environment remote    #
+	#########################################
+
+	def xenv_RemoveXephyrProcess(self, str_display):
+		'''
+		Removes all instances running on display
+		'''
 		
+		display=str_display[1:]
+		# Check if Xephir is running on :display
+		fname='/tmp/.X'+display+'-lock'
+		if (os.path.isfile(fname)):
+			print ("File "+fname+" exists")
+			f = open(fname, 'r')
+			for line in f:
+				if (os.path.exists("/proc/"+line.strip())):
+					print ("Xephir is running on :"+display)
+					os.kill(int(line.strip()), signal.SIGTERM)
+				else:
+					print("File does not exists")
+			os.remove(fname)
+			
+	#def RemoveXephyrProcess():
+	
+	#def xenv_prepare_X11_applications_on_chroot3(self, title):
+	#	print title
+	#	pass
+
+	def get_first_display_free(self):
+		'''
+		Returns firs Display free from 42
+		'''
+		try:
+			display=42
+
+			while(os.path.isfile('/tmp/.X'+str(display)+'-lock')):
+				# There is lock file... is it in use?
+				print ("File "+'/tmp/.X'+str(display)+'-lock'+" exists")
+				f = open('/tmp/.X'+str(display)+'-lock', 'r')
+				for line in f:
+					if (os.path.exists("/proc/"+line.strip())):
+						print ("Any process is running on :"+str(display))
+						# check another display
+						display=display+1
+					else:
+						print("No process locking file, remove lock file")
+						os.remove('/tmp/.X'+str(display)+'-lock')
+						# Return removed display
+						return ":"+str(display)
+
+			return ":"+str(display)
+		except Exception as e:
+			print "Captured: "+str(e)
+			return {'status': False, 'msg':'[LTSPX11Environment] '+str(e)}
+
+
+	def xenv_prepare_X11_applications_on_chroot(self, title, client_display, client_xauthority):
+		'''
+		Prepare a X11 environment to run graphical apps remotely (not necessary in chroot)
+		'''
+		import time
+		try:
+
+			display=self.get_first_display_free();
+
+			# Display on display (first free) with appropiate environment
+			print ("Display...:")
+			my_env=dict(os.environ)
+			my_env['DISPLAY'] = client_display
+			my_env['XAUTHORITY']=client_xauthority
+
+			pid=subprocess.Popen(["Xephyr","-ac",display, "-dpi", "96", "-screen", "400x300x24", "-wr", "-title",title], env=my_env)
+			
+			# Waiting for process
+			while(pid.poll()==False):
+				print "watiting for xephyr"
+				time.sleep(0.5);
+			
+			return [pid, display]
+			
+		except Exception as e:
+			print "Captured: "+str(e)
+			return {'status': False, 'msg':'[LTSPX11Environment] '+str(e)}
+
+	#def xenv_prepare_X11_applications_on_chroot(self,chroot_dir)
+	
+	def xenv_remove_X11_applications_on_chroot(self):
+		'''
+		Kill Xephir Process
+		'''
+		
+		import os
+		import signal
+		
+		try:
+			self.xenv_RemoveXephyrProcess(self.display)
+			
+		except Exception as e:
+			print ("Exception killing:"+str(e))
+			return {'status': False, 'msg':'[N4dChrootAdmin] '+str(e)}
+	#def remove_X11_applications_on_chroot(self,chroot_dir)
+	
+
+	def xenv_prepare_chroot_for_session(self):
+		'''
+		Prepare chroot to start session
+		mounting some directories:
+			* /var/run
+			* ...
+		'''
+		try:
+
+			# Mounting /home on chroot
+			print ("Let's mount home")
+			ret=subprocess.check_output(["mount","--bind","/home/",chroot_dir+"/home/"])
+			print ("Check it!")
+			#ret=subprocess.check_output(["mount","-o","bind","/home",chroot_dir+"/home"])
+
+					# God takes we confessed
+					
+					#ret=subprocess.check_output(["mv", chroot_dir+"/var/run/dbus", chroot_dir+"/var/run/dbus.bak"])
+					#ret=subprocess.check_output(["rm", "-rf", chroot_dir+"/var/run/dbus"])
+					#ret=subprocess.check_output(["ln", "-s", "/var/run/dbus", chroot_dir+"/var/run/dbus"])
+					#ret=subprocess.check_output(["mount","-o","bind","/var/run/dbus",chroot_dir+"/var/run/dbus"])
+
+					#ret=subprocess.check_output(["rm", "-rf", chroot_dir+"/var/lib/dbus/machine-id"])
+					#ret=subprocess.check_output(["ln", "-s", "/var/lib/dbus/machine-id", chroot_dir+"/var/lib/dbus/machine-id"])
+					#ret=subprocess.check_output(["mount","-o","bind","/var/lib/dbus/machine-id",chroot_dir+"/var/lib/dbus/machine-id"])
+
+
+
+
+			# Mount /proc
+			#ret=subprocess.check_output(["mount","-o","bind","/proc",chroot_dir+"/proc"])
+			# Mount /sys
+			#ret=subprocess.check_output(["mount","-o","bind","/sys",chroot_dir+"/sys"])
+			# Mount /dev
+			#ret=subprocess.check_output(["mount","-o","bind","/dev",chroot_dir+"/dev"])
+			# Mount /dev/pts
+			#ret=subprocess.check_output(["mount","-o","bind","/dev/pts",chroot_dir+"/dev/pts"])
+			return True
+		except Exception as e:
+			print("LtspSessionCommands: Not mounted " + str(chroot_dir))
+			return False		
+	#def prepare_chroot_for_run(self,chroot)
+		
+	
+	def xenv_remove_session(self):
+		'''
+		Umount system directories
+		now with -lazy, 
+		TODO:
+			test if it is mounted already
+		'''
+		if not self.test_chroot(chroot_dir)["status"] :
+			# If not a directory...you can't do nothing more.
+			return False
+		else:
+			ret=subprocess.check_output(["umount","-l",chroot_dir+"/home/"])
+			## Mount /proc
+			#ret=subprocess.check_output(["umount","-l",chroot_dir+"/proc"])
+			## Mount /sys
+			#ret=subprocess.check_output(["umount","-l",chroot_dir+"/sys"])
+			## Mount /dev
+			#ret=subprocess.check_output(["umount","-l",chroot_dir+"/dev"])
+			## Mount /dev/pts
+			#ret=subprocess.check_output(["umount","-l",chroot_dir+"/dev/pts"])
+			return True
+	#def remove_session
+		
+
+
+
+		
+
+
 # class lstpClientXServer
